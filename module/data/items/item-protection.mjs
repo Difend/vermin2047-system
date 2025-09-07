ee82bd3d4032186448bd0d782a8ea34b47b54ae6
John CANDELA <john.candela@hotmail.fr>
2025-09-07 18:51:32 +0200

    Added ranks

--- module/data/items/item-protection.mjs
+++ module/data/items/item-protection.mjs
@@ -6,7 +6,27 @@ export default class Vermin2047Protection extends Vermin2047ItemBase {
     const fields = foundry.data.fields;
     const schema = super.defineSchema();
 
-    // TODO
+    schema.index = new fields.SchemaField({
+      value: new fields.NumberField({ required: true, nullable: false, integer: true, initial: 0, min: 0, max: 10 }),
+      special: new fields.SchemaField({
+        value: new fields.NumberField({ required: true, nullable: false, integer: true, initial: 0, min: 0, max: 10 }),
+        type: new fields.StringField({ required: true, blank: false, initial: 'sho' })
+      })
+    });
+
+    schema.reliability = new fields.SchemaField({
+      value: new fields.NumberField({ required: true, nullable: false, integer: true, initial: 1, min: 1, max: 10 }),
+    });
+
+    schema.mobility = new fields.SchemaField({
+      value: new fields.NumberField({ required: true, nullable: false, integer: true, initial: 0, min: 0, max: 3 }),
+    });
+
+    // Defines rarity for the loot actions value (criteria)
+    schema.rarity = new fields.SchemaField({
+      value: new fields.NumberField({ required: true, nullable: false, integer: true, initial: 3, min: 3, max: 10 }),
+      criteria: new fields.NumberField({ required: true, nullable: false, integer: true, initial: 0, min: 0, max: 2 }),
+    });
 
     return schema;
   }
