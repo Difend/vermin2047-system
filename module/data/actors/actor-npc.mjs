29d68b2481f4907420a40caa810caa282b7a2e5c
John CANDELA <john.candela@hotmail.fr>
2025-09-25 15:53:02 +0200

    Allowing attack and damage equals to 0

--- module/data/actors/actor-npc.mjs
+++ module/data/actors/actor-npc.mjs
@@ -7,9 +7,9 @@ export default class Vermin2047NPC extends Vermin2047ActorBase {
     const requiredInteger = { required: true, nullable: false, integer: true };
     const schema = super.defineSchema();
 
-    schema.att = new fields.NumberField({ ...requiredInteger, initial: 1, min: 1 });
+    schema.att = new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 });
     schema.vig = new fields.NumberField({ ...requiredInteger, initial: 1, min: 1 });
-    schema.dam = new fields.NumberField({ ...requiredInteger, initial: 1, min: 1 });
+    schema.dam = new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 });
     
     return schema
   }
